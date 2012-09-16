d2d80ffde1419e99dbc42f430abf91f9b046935c
Cristian Ilea <cristian.ilea@smarttech2000.com>
2012-09-16 16:06:31 +0300

    initial rails app with .rvmrc and .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorChat::Application.config.secret_token = '88be944cccd88b5d9ab5f231882bdf9d7aa4f322266448cad9d95da9d1deaeff79ff65d085ae1da9f03a6711312eab14f20a8722cef7ed5cabc08cd9aeecc25d'
