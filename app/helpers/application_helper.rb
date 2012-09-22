dec43259f32e6b3c55185aca3da3c286de4673e9
Cristian Ilea <cristian.ilea@smarttech2000.com>
2012-09-22 17:37:51 +0300

    fixed login view appearance, added logout btn to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def link_with_icon icon, link, text, klass = "", icon_size = 15
+    link_to "<span style='font-size:#{icon_size}px'<i class='#{icon}'></i></span> #{text}".html_safe, link, :class => "#{klass}"
+  end
 end
